6d4ae27eec68cd3cb9d2448f845d076619cbb58d
Bruno Monte <brunoaguiarprofissional@gmail.com>
2021-08-10 09:53:03 -0300

    Criação Exemplo Json

---
+++ json.js
@@ -0,0 +1,23 @@
+let json = [{
+    "clube": "SPORT RECIFE",
+    "estádio":{
+        "nome": "Ilha do Retiro",
+        "publico_recorde": 65000,
+        "capacidade": null
+    },
+    "jogadores":[{
+        "nome": "André",
+        "idade": 30,
+        "titular": true,
+        "clubes": ["Grêmio, Corinthians"]
+    },
+    {
+        "nome": "Thiago Neves",
+        "idade": 36,
+        "titular": false,
+        "clubes": ["Cruzeiro"]
+    }]
+}];
+
+
+console.log(json);
